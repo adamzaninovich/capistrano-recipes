a395eea544d75bede46c84a5242a8686d9eda3a5
Adam Zaninovich <adam.zaninovich@gmail.com>
2012-04-05 17:15:49 -0700

    cleanup

--- nginx_unicorn_postgres/deploy.rb
+++ nginx_unicorn_postgres/deploy.rb
@@ -26,36 +26,14 @@ ssh_options[:forward_agent] = true
 
 after 'deploy', 'deploy:cleanup' # keep only the last 5 releases
 
-# namespace :deploy do
-#   %w[start stop restart].each do |command|
-#     desc "#{command} unicorn server"
-#     task command, roles: :app, except: {no_release: true} do
-#       run "/etc/init.d/unicorn_#{application} #{command}"
-#     end
-#   end
-#   
-#   task :setup_config, roles: :app do
-#     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-#     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
-#     run "mkdir -p #{shared_path}/config"
-#     put File.read('config/database.yml'), "#{shared_path}/config/database.yml"
-#     puts "Now edit the config files in #{shared_path}."
-#   end
-#   after 'deploy:setup', 'deploy:setup_config'
-#   
-#   task :symlink_config, roles: :app do
-#     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-#   end
-#   after 'deploy:finalize_update', 'deploy:symlink_config'
-#   
+# namespace :deploy
 #   desc 'Make sure local git is in sync with remote.'
 #   task :check_revision, roles: :web do
-#     unless `git rev-parse HEAD` == `git rev-parse github/master`
-#       puts "WARNING: HEAD is not the same as github/master"
+#     unless `git rev-parse HEAD` == `git rev-parse origin/master`
+#       puts "WARNING: HEAD is not the same as origin/master"
 #       puts "Run `git push` to sync changes."
 #       exit
 #     end
 #   end
 #   before 'deploy', 'deploy:check_revision'
-#   
 # end
